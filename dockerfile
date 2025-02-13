82c62f49a2f2621e4f609c4c7ac90ba8e8e594f4
sebastiankz <sebastianolivo@uninorte.edu.co>
2025-02-13 17:35:04 -0500

    codigo dockerfile y .sh terminados

--- dockerfile
+++ dockerfile
@@ -1,5 +1,17 @@
+# Permite ejecutar un contendor de Docker dentro de otro contenedor de Docker
 FROM docker:dind
-RUN apk add --no-cache git bash
+
+#instala git
+RUN apk add --no-cache git
+
+#directorios de trabajo
+WORKDIR /benchmark
+
+#clona el repositorio
+RUN git clone https://github.com/Sebastiankz/benchmark.git .
+
+#copiar el script de ejecución
 COPY execute_all.sh /execute_all.sh
-RUN chmod +x /execute_all.sh
-CMD ["/execute_all.sh"]
+
+#permisos de ejecución
+CMD ["sh", "/execute_all.sh"]
--- execute_all.sh
+++ execute_all.sh
@@ -1,47 +1,11 @@
-#!/bin/bash
-
-# Repositorio de soluciones
-REPOSITORIO="https://github.com/Sebastiankz/benchmark.git"
-DIRECTORIO="benchmark"
-
-# Clonar el repositorio
-git clone $REPOSITORIO $DIRECTORIO || { echo "Error clonando el repositorio"; exit 1; }
-
-# Lenguajes y nombres de las imágenes
-declare -A LANGUAGES
-LANGUAGES=(
-    ["go"]="go-app"
-    ["rust"]="rust-app"
-    ["javascript"]="node-app"
-    ["python"]="python-app"
-    ["c"]="c-app" 
-)
-
-# Crear carpeta de logs y archivo de resultados
-mkdir -p logs
-OUTPUT_FILE="execution_results.txt"
-echo "Comparación de tiempos de ejecución" > $OUTPUT_FILE
-echo "------------------------------------" >> $OUTPUT_FILE
-
-for LANG in "${!LANGUAGES[@]}"; do
-    IMAGE_NAME=${LANGUAGES[$LANG]}
-    LANG_DIR="$DIRECTORIO/$LANG"  # Directorio correcto con la variable
-    LOG_FILE="logs/$LANG/output.txt"
-
-    echo "Construyendo imagen para $LANG..."
-    (cd $LANG_DIR && docker build -t $IMAGE_NAME .) || { echo "Error al construir $LANG"; exit 1; }
-
-    echo "Ejecutando contenedor para $LANG..."
-    mkdir -p "logs/$LANG"
-
-    # Redirigir la salida a output.txt
-    docker run --rm $IMAGE_NAME | tee "$LOG_FILE"
-
-    # Extraer el tiempo de ejecución
-    EXEC_TIME=$(grep -Eo '[0-9]+ ms' "$LOG_FILE")
-    echo "$LANG: $EXEC_TIME" | tee -a $OUTPUT_FILE
+#!/bin/sh
+echo "Lenguaje | Tiempo (ms)" > results.txt
+for lang in c go java javascript python 
+do
+    echo "Ejecutando $lang"
+    docker build -t "$lang - benchmark" "./$lang" 
+    tiempo=$(docker run --rm "$lang - benchmark")
+    echo "$lang | $tiempo ms" >> results.txt
 done
 
-# Mostrar los resultados
-echo "------------------------------------"
-cat $OUTPUT_FILE
+cat results.txt
