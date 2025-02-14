9de88f689c002178ddb9e1b229c587ed71bdc4fa
sebastiankz <sebastianolivo@uninorte.edu.co>
2025-02-13 19:18:31 -0500

    error de tags

--- execute_all.sh
+++ execute_all.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 echo "Lenguaje | Tiempo (ms)" > results.txt
+
 for lang in c go java javascript python 
 do
     echo "Ejecutando $lang"
-    docker build -t "$lang - benchmark" "./$lang" 
-    tiempo=$(docker run --rm "$lang - benchmark")
+    docker build -t "$lang-benchmark" "./$lang"
+    tiempo=$(docker run --rm "$lang-benchmark")
     echo "$lang | $tiempo ms" >> results.txt
 done
 
