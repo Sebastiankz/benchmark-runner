bd3aa5b7595692fb4fe62a7075580781df3f8f96
sebastiankz <sebastianolivo@uninorte.edu.co>
2025-02-13 19:31:06 -0500

    correccion en -sh

--- execute_all.sh
+++ execute_all.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+export DOCKER_HOST="unix:///var/run/docker.sock"
 echo "Lenguaje | Tiempo (ms)" > results.txt
 
 for lang in c go java javascript python 
 do
     echo "Ejecutando $lang"
-    docker build -t "$lang-benchmark" "./$lang"
-    tiempo=$(docker run --rm "$lang-benchmark")
+    docker build -t "$lang-benchmark" "./$lang"  
+    tiempo=$(docker run --rm "$lang-benchmark")  
     echo "$lang | $tiempo ms" >> results.txt
 done
 
